93db3c539eeb0c77cef4e851b5a8f3e06f6b297a
chet@n <chetan.simplyfying@gmail.com>
2020-03-28 00:02:08 +0530

    thiird commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/svg/LogoIcon.js`
+        // icon: `src/images/logo.png`
       }
     },
     `gatsby-plugin-postcss`,
